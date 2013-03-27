824bad9e4820155a6c7400673d7b734b8ba1b99e
Chapman Shoop <chapman.shoop@gmail.com>
2013-03-27 07:21:53 -0700

    pushing up what I've got for now

--- spec/clj_memo/core_spec.clj
+++ spec/clj_memo/core_spec.clj
@@ -3,47 +3,57 @@
             [clj-time.core :as clj-time]
             [clj-memo.core :refer :all]))
 
+(defn add-to-schedule [card review-window]
+  (swap! card update-in [:schedule] conj review-window))
+
 (describe "card"
   (it "should have a question, answer, schedule and easy factor"
-    (let [new-card (create-card "question" "answer")]
+    (let [new-card @(create-card "question" "answer")]
       (should= "question" (:question new-card))
       (should= "answer" (:answer new-card))
       (should= 2.5 (:easy-factor new-card))
       (should= [] (:schedule new-card))))
   
   (it "should know when it's due for review"
-    (let [card (assoc (create-card "q" "a") :schedule [{:from (clj-time/date-time 2000 1 1)
-                                                         :review-within-days 1}])]
+    (let [card (create-card "q" "a")]
+      (add-to-schedule card {:from (clj-time/date-time 2000 1 1) :review-within-days 1})
       (should= (clj-time/date-time 2000 1 2) (review-card-by-date card))))
-)
+
+  (it "should update when reviewed"
+    (let [card (atom {:easy-factor 2.5 :schedule []})
+          reviewed-card (review-card! card 3)]
+      (should= 2.36 (:easy-factor reviewed-card)))
+    )
+  )
+
+(describe "Training program"
+  )
 
 (describe "SuperMemo2 card scheduling"
   (it "should schedule a new card to be reviewed within a day"
     (let [new-card (create-card "question" "answer")]
-      (should= 1 (review-card-within-days new-card))))
+      (should= 1 (next-review-interval new-card))))
 
   (it "should schedule the second review within six days"
     (let [card (create-card "question" "answer")]
-      (should= 6 (review-card-within-days 
-                  (assoc card :schedule [{:from "start-time"
-                                          :review-within-days 1}])))))
+      (add-to-schedule card {:from (clj-time/date-time 2000 1 1) :review-within-days 1})
+      (should= 6 (next-review-interval card))))
 
   (it "should use the SM2 update equation from iteration 3 on"
-    (let [card (create-card "question" "answer")
-          card (assoc card :schedule [{:from "start-time"
-                                       :review-within-days 1}
-                                      {:from "start-time"
-                                       :review-within-days 6}])]
-      (should= 15 (review-card-within-days card)))))
+    (let [card (create-card "question" "answer")]
+      (add-to-schedule card {:from (clj-time/date-time 2000 1 1) :review-within-days 1})
+      (add-to-schedule card {:from (clj-time/date-time 2000 1 1) :review-within-days 6})
+      (should= 15 (next-review-interval card)))))
 
 (describe "Card deck operations"
   (it "should be able to get the next scheduled card"
     (let 
-        [soon-card (assoc (create-card  "q" "a") :schedule [{:from (clj-time/date-time 2000 1 1)
-                                                             :review-within-days 1}])
-         later-card (assoc (create-card "q" "a") :schedule [{:from (clj-time/date-time 2000 1 1)
-                                                             :review-within-days 2}])
+        [soon-card (create-card  "q" "a")
+         later-card (create-card "q" "a")
          deck [soon-card later-card]]
+      (add-to-schedule soon-card {:from (clj-time/date-time 2000 1 1) :review-within-days 1})
+      (add-to-schedule later-card {:from (clj-time/date-time 2000 1 1) :review-within-days 6})
+
       (should= soon-card (next-card-to-review deck)))))
 
 (run-specs)
--- src/clj_memo/core.clj
+++ src/clj_memo/core.clj
@@ -1,30 +1,81 @@
 (ns clj-memo.core
-  (:require [clj-time.core :as clj-time]))
+  (:require [clj-time.core :as clj-time]
+            [clj-time.local :refer :all]))
 
-(def card-deck [])
+(def ^:dynamic *card-deck* [])
 
 (defn create-card
   "Create a new card."
   [question answer]
-  {:question question 
-   :answer answer
-   :easy-factor 2.5
-   :schedule []})
+  (atom {:question question 
+         :answer answer
+         :easy-factor 2.5
+         :last-review-date nil
+         :next-review-date nil
+         :high-quality-reviews 0}))
+
+(defn next-easy-factor [last-easy-factor recall]
+  (+ last-easy-factor (- 0.1 (* (- 5 recall) (+ 0.08 (* (- 5 recall) 0.02))))))
+
+(defn review-card! [card recall]
+  (swap! card update-in [:easy-factor] next-easy-factor recall))
+
+(defn next-review-interval
+  "Determine how many days to review the card within."
+  [card]
+  (let [{:keys [easy-factor schedule]} @card]
+    (cond 
+     (= (count schedule) 0) 1
+     (= (count schedule) 1) 6
+     :else (int (* (:review-within-days (last schedule)) easy-factor)))))
+
+(defn review-card [card recall]
+  (let [card @card]
+    (update-in card [:easy-factor] next-easy-factor recall)
+    (update-in card [:schedule] conj {:from (local-now) 
+                                      :review-within-days (next-review-interval)})))
 
 (defn review-card-by-date
   "Determine the date by which the card should be reviewed."
   [card]
-  (clj-time/plus (:from (last (:schedule card))) (clj-time/days (:review-within-days (last (:schedule card))))))
+  (clj-time/plus (:from (last (:schedule @card))) (clj-time/days (:review-within-days (last (:schedule @card))))))
 
-(defn review-card-within-days
-  "Determine how many days to review the card within."
+
+(comment
+  (defn main
+    "Run the training program"
+    []
+    ;; Get the cards that need to be reviewed today
+    ;; Show cards and calculate next review date
+    (doseq [card (cards-to-review (local-now))]
+      (review-card card))
+    ))
+
+(comment
+  (review-card
+   [card]
+   (let [recall (prompt-card card)]
+     (update-card card recall))))
+
+
+(comment
+  (defn cards-to-review
+    "Return the collection of cards that need to be reviewed today"
+    [deck date]
+    (filter (partial date review-card?) deck)))
+
+(defn review-card? [date card]
+  (let [{:keys [next-review-date]} @card]
+    (if (nil? next-review-date) true
+        (clj-time/before? next-review-date date))))
+
+(defn prompt-card
+  "Display card at prompt and return recall."
   [card]
-  (cond 
-   (= (count (:schedule card)) 0) 1
-   (= (count (:schedule card)) 1) 6
-   :else (int (* (:review-within-days (last (:schedule card))) (:easy-factor card)))))
-
-(defn next-card-to-review
-  "Find the card with the closest due date."
-  [deck]
-  (first (sort-by review-card-by-date deck)))
+  (let [{:keys [question answer]} @card]
+    (println question)
+    (println "Hit enter for answer...")
+    (read-line)
+    (println answer)
+    (println "How would you rate your recall:")
+    (read)))
